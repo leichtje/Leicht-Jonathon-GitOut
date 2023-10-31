add571be91c1ffd4c1f0ad020f3f904df531caa3
leichtje <113708612+leichtje@users.noreply.github.com>
2023-10-31 10:30:16 -0400

    Add project files.

---
+++ Leicht Jonathon GitOut/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Leicht_Jonathon_GitOut.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
